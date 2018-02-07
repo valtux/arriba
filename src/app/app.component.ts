e12c6bbc12d1031da62dfcca116072b553c25e55
tuxval <valeriashanahan@gmail.com>
2018-02-07 09:13:17 -0300

    modificacion en el titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'BASTA DE BIENVENIDA. ESTO SE TIENE QUE VER EN EL SERVIDOR!!!!!';
 }
